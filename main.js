4d335e9ae72e6429ca8f4a6cb5a86f8337b4481b
Carlos Henrique <rapaiva21@gmail.com>
2021-11-09 17:19:30 -0300

    Foi atribuido ao input date uma variável em JS com o resultado de mostrar a data em uma outra variável, porém a data não está sendo mostrada da maneira que queremos

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const criarTarefa = (evento)=> {
   const input = document.querySelector('[data-form-input]')
   const valor = input.value
 
+  const calendario = document.querySelector('[data-form-date]')
+  const data = calendario.value
+  console.log(data)
+
   const tarefa = document.createElement('li')
   tarefa.classList.add("task")
   const conteudo = `<p class="content">${valor}</p>`
