a119c5b3d38f9ce3858541a6febc038ea6ef1e40
Carlos Henrique <rapaiva21@gmail.com>
2021-11-11 12:53:29 -0300

    A primeira parte foi adicionar a data para aparecer no html. Para melhorar o código, vamos refatorar o código: Primeiro, vamos dividir a funcção criarTarefa para uma trabalhar com dados e outra trabalhar com visualização. unto com um monte de coisa... AUla é complicada, mas as mudanças estão aí

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
 
-const criarTarefa = (evento)=> {
+const handleNovoItem = (evento)=> {
   evento.preventDefault()
-
   const lista = document.querySelector('[data-list]')
   const input = document.querySelector('[data-form-input]')
   const valor = input.value
@@ -11,18 +10,32 @@ const criarTarefa = (evento)=> {
   const calendario = document.querySelector('[data-form-date]')
   const data = moment(calendario.value)
   
-  console.log(data.format('DD/MM/YYYY'))
+  const dataFormatada = data.format('DD/MM/YYYY')
 
-  const tarefa = document.createElement('li')
-  tarefa.classList.add("task")
-  const conteudo = `<p class="content">${valor}</p>`
-  tarefa.innerHTML = conteudo
+  const dados = {
+    valor, 
+    dataFormatada
+  }
 
-  tarefa.appendChild(BotaoDeleta())
-  tarefa.appendChild(BotaoConclui())
-  lista.appendChild(tarefa)
+  const criaTarefa = criarTarefa(dados)
+
+  lista.appendChild(criaTarefa)
   input.value = " "
 }
 
+const criarTarefa = ({ valor, dataFormatada })=> {
+  const tarefa = document.createElement('li')
+  tarefa.classList.add("task")
+  const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+  tarefa.innerHTML = conteudo
+
+ 
+  tarefa.appendChild(BotaoDeleta())
+  tarefa.appendChild(BotaoConclui())
+
+  return tarefa
+  
+}
+
 const novaTarefa = document.querySelector('[data-form-button]')
-novaTarefa.addEventListener("click" , criarTarefa)
+novaTarefa.addEventListener("click" , handleNovoItem)
