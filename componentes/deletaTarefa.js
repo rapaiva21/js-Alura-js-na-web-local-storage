ccfb47a5c54c87244ad359ac0f60d9f715cc66c8
Carlos Henrique <rapaiva21@gmail.com>
2021-11-14 17:02:35 -0300

    Adicionado o carregaTarefa() dentro do main para carregar sempre que o botao novaTarefa foi acionado.

--- componentes/deletaTarefa.js
+++ componentes/deletaTarefa.js
@@ -9,6 +9,8 @@ const deletarTarefa = (evento)=> {
   const botaoDeleta = evento.target
   const tarefaCompleta = botaoDeleta.parentElement
   tarefaCompleta.remove()
+
+  return botaoDeleta
 }
 
 export default BotaoDeleta
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-import { handleNovoItem } from "./componentes/criaTarefa.js"
+import {handleNovoItem} from './componentes/criaTarefa.js'
+import {carregaTarefa} from './componentes/carregaTarefa.js'
 const novaTarefa = document.querySelector('[data-form-button]')
-novaTarefa.addEventListener("click" , handleNovoItem)
+
+novaTarefa.addEventListener('click', handleNovoItem)
+
+carregaTarefa()
